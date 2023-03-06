6a33a5a6fa4f05e208ae39a906d019603a5be7b1
James Rupa <jamesjrupa@gmail.com>
2023-03-05 19:56:51 -0500

    Added Player and Mouse Scripts to Player Object

---
+++ LostHaste/Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+
+    // variables
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // first person pov - locks cursor at midpoint of screen and makes it invisible
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // looking up and down (x and y axis)
+        xRotation -= mouseY;
+        yRotation += mouseX;
+
+        // prevent over rotation
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
