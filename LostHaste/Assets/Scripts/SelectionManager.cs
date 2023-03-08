e33f62acb9f3689c6bde144369dfecf8b3325ba4
James Rupa <jamesjrupa@gmail.com>
2023-03-07 19:52:51 -0500

    Added Simple Interactive UI & Scripts

---
+++ LostHaste/Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionManager : MonoBehaviour
+{
+
+    public GameObject interactionInfoUI;
+    Text interactionText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactionText = interactionInfoUI.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // raycast points to center of screen
+        // where mouse pointer is locked
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // if something is hit with raycast
+        // distance set to 2.5f
+        if(Physics.Raycast(ray, out hit, 2.5f)) {
+            var selectionTransform = hit.transform;
+
+            // if object has interactable script
+            // change text
+            if(selectionTransform.GetComponent<InteractableObject>()) {
+
+                interactionText.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                interactionInfoUI.SetActive(true);
+            } else {
+                interactionInfoUI.SetActive(false);
+            }
+            // if no longer looking at object turn off InteractionInfoUI
+        } else {
+            interactionInfoUI.SetActive(false);
+        }
+    }
+}
