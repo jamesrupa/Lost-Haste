b58db2dff5846e52adf7adc7c09221804951e625
James Rupa <jamesjrupa@gmail.com>
2023-05-08 14:07:36 -0400

    Main Menu, In Game Menu, & Menu Systems

---
+++ LostHaste/Assets/Scripts/Menus/InGameMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    public void BackToMainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ LostHaste/Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public static MenuManager Instance{
+        get;
+        set;
+    }
+
+    // var
+    public GameObject menuCanvas;
+    public GameObject uiCanvas;
+
+    public GameObject saveMenu;
+    public GameObject settingsMenu;
+    public GameObject menu;
+
+    public bool isMenuOpen;
+
+    private void Awake() {
+        if(Instance != null && Instance != this) {
+            Destroy(gameObject);
+        } else {
+            Instance = this;
+        }
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.M) && !isMenuOpen) {
+            uiCanvas.SetActive(false);
+            menuCanvas.SetActive(true);
+
+            isMenuOpen = true;
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            SelectionManager.Instance.DisableSelection();
+            SelectionManager.Instance.GetComponent<SelectionManager>().enabled = false;
+        } else if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.M) && isMenuOpen) {
+
+            // resets menu for next time
+            saveMenu.SetActive(false);
+            settingsMenu.SetActive(false);
+            menu.SetActive(true);
+
+            uiCanvas.SetActive(true);
+            menuCanvas.SetActive(false);
+
+            isMenuOpen = false;
+
+            if(!CraftingSystem.Instance.isOpen && !InventorySystem.Instance.isOpen) {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+
+            SelectionManager.Instance.EnableSelection();
+            SelectionManager.Instance.GetComponent<SelectionManager>().enabled = true;
+        }
+    }
+}
